74d597191598503c77951b040c3474a4c87f0960
Frank Collins <fwc3rd@gmail.com>
2023-06-27 03:55:29 -0400

    tokenize callback [3:55am]

--- AaA_bBb.ino
+++ AaA_bBb.ino
@@ -1,11 +1,11 @@
-#define MAX_WORDS_LENGTH 100
-
+#define MAX_WORDS_LENGTH 13
+#define MAX_WORDS 20
 #define LUCKY 13
 
 String sen;
 String uppercasetoggle;
 String lowercasetoggle;
-int word_index = 0;
+int tokenize_yet = 0;
 
 struct Word {
   int length;
@@ -13,7 +13,8 @@ struct Word {
   struct Word *next;
 };
 
-struct Word words[MAX_WORDS_LENGTH];
+typedef struct Word Word;
+Word words[MAX_WORDS];
 
 struct Word *head = nullptr;
 struct Word *end = nullptr;
@@ -27,6 +28,11 @@ void setup() {
 
 void loop() {
   // Your loop code here
+  
+  if (tokenize_yet == 0) {
+   	Serial.println("hey tokenize once!");
+    tokenize_yet = 1;
+  }
 }
 
 
@@ -48,4 +54,21 @@ void print_words()
 //		Serial.println(head->prefix);
     }
   }
+}
+
+void tokenize(String usersentence) 
+{
+ int word_index = 0;
+ int i = 0;
+ int len = usersentence.length();
+ 
+  for (i; i < len; i++) {
+    char c = usersentence[i];
+    if (c == ' ' || c == '\n') {
+		words[i].length = word_index;
+ strncpy(words[word_index].prefix, &usersentence[i],
+              words[word_index].length >= 4 ? 4 : words[word_index].length);
+    }
+  }
+  
 }
